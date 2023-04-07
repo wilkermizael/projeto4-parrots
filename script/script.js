73eca7a3954829aa467fd90cd724fb9f0bce541f
Wilker <wilkermizael@gmail.com>
2023-04-07 09:43:44 -0300

    Funcionalidade de virar as cartas

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 /* PEDIR AO USUÁRIO A QUANTIDADE DE CARTAS QUE ELE DESEJA, DE ACORDO COM AS CONDIÇÕES*/
-/*let num = prompt("Por favor, insira a quantidade de cartas que deseja jogar:");
+let num = prompt("Por favor, insira a quantidade de cartas que deseja jogar:");
 solicitaQtddeCartas();
 
 function solicitaQtddeCartas(){
@@ -7,25 +7,71 @@ while ( num % 2 !== 0 || num < 4 || num >14) {
   num = prompt("Você não inseriu um número par. Por favor, insira um número par entre 4 e 14");
 }
 
-console.log(num);
+
 }
 
-*/
+addCards();
 
+const array=[
+  'bobrossparrot.gif', 
+  'explodyparrot.gif',
+  'fiestaparrot.gif',
+  'metalparrot.gif',
+  'revertitparrot.gif',
+  'tripletsparrot.gif',
+  'unicornparrot.gif'
+];
+sorteando(array);
+
+console.log(myArray);
+
+//console.log(array)
 
 // Aplicar virar a carta com o click---------------------------------------------------------------------------------------------------------------------
-function turnCard(){
-    const carta1 = document.querySelector('.carta1');
-    carta1.classList.toggle("front");
 
-    const carta2 = document.querySelector('.carta2');
-    carta2.classList.toggle("back");
+function clickTurnCard(carta){
+  
+
+    const carta1= carta.querySelector('.carta1');
+    carta1.classList.toggle('front');
+
+    const carta2 = carta.querySelector('.carta2');
+    carta2.classList.toggle('back');
+  
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Aplicar dinamicamente a quantidade de cartas passadasd pelo Usuario-----------------------------------------------------------------------------------
+// Aplicar dinamicamente a quantidade de cartas passadas pelo Usuario-----------------------------------------------------------------------------------
+
+function addCards(){
+    const listOfCards = document.querySelector('.allCards');
+    
+   for(let i = 0; i < num; i++){
+
+   
+      listOfCards.innerHTML +=  `<ul onclick="clickTurnCard(this)" class="card">
+            
+                                  <li class="carta1 face ">
+                                    <img src="../imagens/back.png">
+                                  </li>
+
+                                  <li class="carta2 back-face face">
+                                    Verso
+                                  </li>
+
+                                  </ul>`;
+    }
+
+  
+}
 
 
+//Aplicar as cartas no verso de maneira aleatória--------------------------------------------------------------------------------------------------------
+const myArray= [];
+function sorteando(este){
+  for( let i = 0; i < num; i++){
+    myArray.push(este[i]);
+  }
 
+}
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
