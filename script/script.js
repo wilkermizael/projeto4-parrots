552871a8e7527df14f03b2b6c411686ae5221f3c
Wilker <wilkermizael@gmail.com>
2023-04-08 16:11:55 -0300

    Verifica se as cartas sao iguais

--- script/script.js
+++ script/script.js
@@ -9,10 +9,17 @@ while ( num % 2 !== 0 || num < 4 || num >14) {
 
 
 }
+//------------------------------------------------------------------------------------------------------------------------------------
+let carta1='';
+let carta2 ='';
+let primeiraCarta='';
+let segundaCarta = '';
+let contadorJogadas = 0;
+let contadorClick = 0;
+let contadorDeParesVirados =0;
 
-addCards();
 
-const array=[
+const gifs=[
   'bobrossparrot.gif', 
   'explodyparrot.gif',
   'fiestaparrot.gif',
@@ -21,33 +28,90 @@ const array=[
   'tripletsparrot.gif',
   'unicornparrot.gif'
 ];
-sorteando(array);
 
-console.log(myArray);
 
-//console.log(array)
+// Sortear as gifs-------------------------------------------------------------------------------------------------------------------------------------
+const arrayParaSortear = [];
+for( let i =0; i < num/2; i++){
+  arrayParaSortear.push(gifs[i]);
+  arrayParaSortear.push(gifs[i]);
+}
+arrayParaSortear.sort(comparador); 
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+// Função que impede mais de dois clicks----------------------------------------------------------------------------------------------------------------
+
+
 
 // Aplicar virar a carta com o click---------------------------------------------------------------------------------------------------------------------
 
 function clickTurnCard(carta){
   
+     carta1= carta.querySelector('.carta1');
+     carta2 = carta.querySelector('.carta2');
+    
+    if(carta1.classList.contains('front') !== true && contadorClick === 0){
+      contadorClick++;
+      primeiraCarta = carta.querySelector('.carta2');
+      contadorJogadas++;
 
-    const carta1= carta.querySelector('.carta1');
-    carta1.classList.toggle('front');
+      carta1.classList.add('front');
+      carta2.classList.add('back');
+    } else if(carta1.classList.contains('front') !== true && contadorClick ===1){
+      
+      contadorClick++;
+      contadorJogadas++;
+      segundaCarta = carta.querySelector('.carta2');
+      carta1.classList.add('front');
+      carta2.classList.add('back');
 
-    const carta2 = carta.querySelector('.carta2');
-    carta2.classList.toggle('back');
+      //comparaCarta(primeiraCarta , segundaCarta);
+      setTimeout(comparaCarta, 1000, primeiraCarta, segundaCarta);
+
+    }
+
   
 }
+// Função que compara as imagens do verso-----------------------------------------------------------------------------------
 
+function comparaCarta(a,b){
+  const asrc = a.querySelector('img').src;
+  const bsrc = b.querySelector('img').src;
+  if(asrc === bsrc){
+    contadorDeParesVirados++;
+
+    contadorClick =0;
+    primeiraCarta =0;
+    segundaCarta =0;
+  }else if(asrc !== bsrc){
+    a.classList.remove('back');
+    b.classList.remove('back');
+    const aParent = a.parentNode.querySelector('.carta1')
+    const bParent = b.parentNode.querySelector('.carta1')
+
+    aParent.classList.remove('front');
+    bParent.classList.remove('front');
+
+    contadorClick =0;
+    primeiraCarta =0;
+    segundaCarta =0;
+  }
+  if(contadorDeParesVirados == num/2){
+    alert(`Você ganhou em ${contadorJogadas} jogadas!`)
+  }
+}
+// Aplicar setTimeOut-----------------------------------------------------------------------------------
 
 // Aplicar dinamicamente a quantidade de cartas passadas pelo Usuario-----------------------------------------------------------------------------------
 
 function addCards(){
     const listOfCards = document.querySelector('.allCards');
-    
-   for(let i = 0; i < num; i++){
-
+   
+    for(let i = 0; i < num; i++){
+      
    
       listOfCards.innerHTML +=  `<ul onclick="clickTurnCard(this)" class="card">
             
@@ -56,22 +120,17 @@ function addCards(){
                                   </li>
 
                                   <li class="carta2 back-face face">
-                                    Verso
+                                    <img src="./imagens/${arrayParaSortear[i]}">
                                   </li>
 
                                   </ul>`;
     }
 
-  
-}
-
-
-//Aplicar as cartas no verso de maneira aleatória--------------------------------------------------------------------------------------------------------
-const myArray= [];
-function sorteando(este){
-  for( let i = 0; i < num; i++){
-    myArray.push(este[i]);
-  }
+    //Lista de Cartas do verso.
 
 }
+
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+addCards();
